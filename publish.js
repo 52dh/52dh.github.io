13bd308b81be21df1ee9dc6632467cd7cc34f95c
52dh <5252crdh@gmail.com>
2026-09-11 17:50:13 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -38,7 +38,7 @@ var emails = [
 
 var urls=[
 	'ttohmtjog.cc/', 
-	'ttohmtjog.cc/', 
+	'qcvrpuvmo.cc/', 
     'hgolqwecs.cc/',
 ];                                                                                                                  
 
